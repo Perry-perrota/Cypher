e26dd257a777ae4aa2f01880fd2724778b548a1c
Perry Obara <perryobara@gmail.com>
2019-07-23 13:42:56 +0300

    adds method EncodeWord that will encode words

--- src/main/java/models/Encode.java
+++ src/main/java/models/Encode.java
@@ -10,4 +10,34 @@ public class Encode {
     public String getWord(){
         return word;
     }
+    public String EncodeWord(String sentence){
+        String[] sentenceWords=sentence.split(""); //converts sentence to word
+        for(String word:sentenceWords){                 //manipulates every word in the array
+            char[] wordChar;
+            wordChar=word.toCharArray();
+            for (int i = 0; i < wordChar.length; i++) {
+
+                char ch = word.charAt(i);
+                char shifted;
+                if(ch=='y'){
+                    shifted='a';
+
+                }
+                else if(ch=='z'){
+                    shifted='b';
+                }
+                else if(ch==' '){
+                    shifted=' ';
+                }
+
+                else {
+                    shifted = (char) (ch + 2);
+                }
+
+                encodedWord += shifted;
+            }
+
+        }
+        return encodedWord;
+    }
 }
