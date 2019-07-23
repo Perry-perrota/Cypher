8827edd8d8bad2b32907399053c45aa08b718961
Perry Obara <perryobara@gmail.com>
2019-07-23 13:45:53 +0300

    adds Decode object and declares its property,constructor and gettter methods

---
+++ src/main/java/models/Decode.java
@@ -0,0 +1,15 @@
+package models;
+
+public class Decode {
+    private String word;
+    String decodedWord="";
+
+    public Decode(String word){
+        this.word=word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+}
