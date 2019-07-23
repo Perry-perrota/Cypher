f0867580fc4a1b811c132882dbe7baa8a1a53ea5
Perry Obara <perryobara@gmail.com>
2019-07-23 13:47:45 +0300

    adds DecodeWord method that will decode the given words

--- src/main/java/models/Decode.java
+++ src/main/java/models/Decode.java
@@ -11,5 +11,32 @@ public class Decode {
     public String getWord() {
         return word;
     }
+    public String DecodeWord(String sentence) {
+        String[] sentenceWords = sentence.split(""); //converts sentence to word
+        for (String word : sentenceWords) {                 //manipulates every word in the array
+            char[] wordChar;
+            char shifted;
+            wordChar = word.toCharArray();
+            for (int i = 0; i < wordChar.length; i++) {     //manipulates individual characters in a word
 
+                char ch = word.charAt(i);
+                if (ch == 'a') {
+                    shifted = 'y';
+
+                } else if (ch == 'b') {
+                    shifted = 'z';
+
+                }else if(ch==' '){
+                    shifted=' ';
+                }
+                else {
+                    shifted = (char) (ch - 2);
+
+                }
+                decodedWord += shifted;
+            }
+
+        }
+        return decodedWord;
+    }
 }
