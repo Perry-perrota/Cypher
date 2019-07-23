1951402432a5e54b8d5218ffc3e24c451676e63f
Perry Obara <perryobara@gmail.com>
2019-07-23 13:54:11 +0300

    adds front end to gather user input from terminal

---
+++ src/main/java/App.java
@@ -0,0 +1,37 @@
+import models.*;
+
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean running = true;
+        while (running) {
+            System.out.println("Kindly input a word");
+            String word = input.nextLine();
+            System.out.println("Select one of the following");
+            System.out.println("1:Encode");
+            System.out.println("2.Decode");
+            String choice = input.nextLine();
+            int choiceInput = Integer.parseInt(choice);
+
+
+            if (choiceInput == 1) {
+                Encode newTry = new Encode(word);
+                String encodedWord = newTry.EncodeWord(word);
+                System.out.println(encodedWord);
+
+            } else if (choiceInput == 2) {
+                Decode newTry = new Decode(word);
+                String decodedWord = newTry.DecodeWord(word);
+                System.out.println(decodedWord);
+
+            } else {
+                running = false;
+            }
+
+
+        }
+    }
+}
