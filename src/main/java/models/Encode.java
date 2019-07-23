8d6c2f39e2724023a28e9ac958c4587d1d2b873b
Perry Obara <perryobara@gmail.com>
2019-07-23 13:41:42 +0300

    adds Encode object and declares its properties,constructor and getter method

---
+++ src/main/java/models/Encode.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Encode {
+    private String word;
+    String encodedWord="";
+
+    public Encode(String word){
+        this.word=word;
+    }
+    public String getWord(){
+        return word;
+    }
+}
